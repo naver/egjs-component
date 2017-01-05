7518de727276258c6381e82e1183c440b7635631
Taihoon Kim <kishu@navercorp.com>
2017-01-05 16:32:49 +0900

    chore: update webpack config. apply multi part library
    see also https://github.com/webpack/webpack/tree/master/examples/multi-part-library

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,35 @@ var webpack = require("webpack");
 var path = require('path');
 
 module.exports = {
-  entry: {
-    "eg.component": "./src/index.js",
-    "eg.component.min": "./src/index.js",
-    "eg.component.test": "./test/index.js",
-  },
-  devtool: "source-map",
-  output: {
-    path: './dist',
-    filename: '[name].js',
-    library: "eg",
-    libraryTarget: "umd"
-  },
-  module: {
-    loaders: [
-      {
-        test: [path.join(__dirname, 'src/'), path.join(__dirname, 'test/')],
-        loader: "babel-loader",
-        query: {
-          presets: ['es2015']
-        }
-      }
-    ]
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      include: /\.min\.js$/,
-      minimize: true
-    })
-  ]
+	entry: {
+		"eg.component": "./src/index.js",
+		"eg.component.min": "./src/index.js",
+		"eg.component.test": "./test/index.js",
+	},
+	output: {
+		path: __dirname + "/dist",
+		filename: "[name].js",
+		library: ["eg", "component"],
+		libraryTarget: "umd"
+	},
+	devServer: {
+		publicPath: "/dist/"
+	},
+	devtool: "source-map",
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: "babel-loader",
+			options: {
+				presets: ["es2015"]
+			}
+		}]
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+			include: /\.min\.js$/,
+			minimize: true
+		})
+	]
 };
