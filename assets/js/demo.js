f7ec290fbae0af35bea763ff6683cc1a6fc16998
sculove <sculove@gmail.com>
2017-08-17 17:09:41 +0900

    Updates

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,3 +1,44 @@
-$(function() {
-/** js **/
-});
+
+window.onload = function(){
+	function Some(){
+
+	}
+	Some.prototype = new eg.Component(); //extends
+	Some.prototype.constructor = Some;
+	Some.prototype.foo = function(){
+		this.trigger("hi");// fire hi event.
+	}
+
+	var some = new Some();
+
+
+	function desc(str) {
+		document.getElementById('desc').innerText = str;
+	}
+	function desc2(str) {
+		document.getElementById('desc2').innerText = str;
+	}
+	var count = 0;
+	function fired(){
+		count++;
+		desc2(count+" fired hi event");
+	}
+
+	document.getElementById('btn_trigger').addEventListener("click",function(e) {
+		e.preventDefault();
+		some.foo();
+		desc("'hi' event trigger.");
+	});
+
+	document.getElementById('btn_attach').addEventListener("click",function(e) {
+		e.preventDefault();
+		some.on("hi",fired);
+		desc("'hi' event on.");
+	});
+
+	document.getElementById('btn_detach').addEventListener("click",function(e) {
+		e.preventDefault();
+		some.off("hi",fired);
+		desc("'hi' event off.");
+	});
+}
