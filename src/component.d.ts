6330277aef43f05b5e84bb245bce11c51eb9e0bc
Son Chan Uk <sculove@gmail.com>
2017-08-28 10:36:41 +0900

    chore(component.d.ts): sync definition file with types (#23)
    ref #21

--- src/component.d.ts
+++ src/component.d.ts
@@ -1,7 +1,8 @@
-// Type definitions for egjs-component 2.0.0+
+// Type definitions for egjs-component 2.0
 // Project: https://github.com/naver/egjs-component
 // Definitions by: Naver <https://github.com/naver>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
 
 export as namespace eg;
 
@@ -12,22 +13,17 @@ declare class Component {
   hasOn(eventName: string): boolean;
   on(eventName: string, handlerToAttach: (event: { [key: string]: any }) => any): Component;
   on(events: { [key: string]: (event: { [key: string]: any }) => any }): Component;
-  off(eventName: string, handlerToAttach: (event: { [key: string]: any }) => any): Component;
-  off(eventName: string): Component;
-  off(): Component;
+  off(eventName?: string, handlerToAttach?: (event: { [key: string]: any }) => any): Component;
   once(events: { [key: string]: (event: { [key: string]: any }) => any }): Component;
   once(eventName: string, handlerToAttach: (event: { [key: string]: any }) => any): Component;
-  options:  { [key: string]: any };
+  options: { [key: string]: any };
 }
-
-declare module Component {
+declare namespace Component {
   function trigger(eventName: string, customEvent?: { [key: string]: any }): boolean;
   function hasOn(eventName: string): boolean;
   function on(eventName: string, handlerToAttach: (event: { [key: string]: any }) => any): Component;
   function on(events: { [key: string]: (event: { [key: string]: any }) => any }): Component;
-  function off(eventName: string, handlerToAttach: (event: { [key: string]: any }) => any): Component;
-  function off(eventName: string): Component;
-  function off(): Component;
+  function off(eventName?: string, handlerToAttach?: (event: { [key: string]: any }) => any): Component;
   function once(events: { [key: string]: (event: { [key: string]: any }) => any }): Component;
   function once(eventName: string, handlerToAttach: (event: { [key: string]: any }) => any): Component;
   let options: { [key: string]: any };
