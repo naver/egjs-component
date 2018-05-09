70e89855ac55da6f6ed9407562a161b9beeb999c
YongWoo Jeon <yongwoo.jeon@navercorp.com>
2018-05-09 14:08:28 +0900

    chore(docs): Add version description for docs.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import Component from "./Component";
 
+/**
+ * Version info string
+ * @ko 버전정보 문자열
+ * @name VERSION
+ * @static
+ * @type {String}
+ * @example
+ * eg.Component.VERSION;  // ex) 2.0.0
+ * @memberof eg.Component
+ */
 Component.VERSION = "#__VERSION__#";
 
 export default Component;
