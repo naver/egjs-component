4dbd0763780d65ac836027c1c51ffacf23f95adf
WoodNeck <saiyu915@naver.com>
2020-10-14 13:46:34 +0900

    chore(release): Release 2.2.0

---
+++ dist/component.js
@@ -0,0 +1,266 @@
+/*
+Copyright (c) NAVER Corp.
+name: @egjs/component
+license: MIT
+author: NAVER Corp.
+repository: https://github.com/naver/egjs-component
+version: 2.2.0
+*/
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global.eg = global.eg || {}, global.eg.Component = factory());
+}(this, (function () { 'use strict';
+
+    /*! *****************************************************************************
+    Copyright (c) Microsoft Corporation.
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose with or without fee is hereby granted.
+
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+    REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+    INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+    LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+    OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+    PERFORMANCE OF THIS SOFTWARE.
+    ***************************************************************************** */
+    function __values(o) {
+      var s = typeof Symbol === "function" && Symbol.iterator,
+          m = s && o[s],
+          i = 0;
+      if (m) return m.call(o);
+      if (o && typeof o.length === "number") return {
+        next: function () {
+          if (o && i >= o.length) o = void 0;
+          return {
+            value: o && o[i++],
+            done: !o
+          };
+        }
+      };
+      throw new TypeError(s ? "Object is not iterable." : "Symbol.iterator is not defined.");
+    }
+
+    /*
+     * Copyright (c) 2015 NAVER Corp.
+     * egjs projects are licensed under the MIT license
+     */
+
+    function isUndefined(value) {
+      return typeof value === "undefined";
+    }
+    /**
+     * A class used to manage events in a component
+     * @ko 컴포넌트의 이벤트을 관리할 수 있게 하는 클래스
+     * @alias eg.Component
+     */
+
+
+    var Component =
+    /*#__PURE__*/
+    function () {
+      /**
+       * @support {"ie": "7+", "ch" : "latest", "ff" : "latest",  "sf" : "latest", "edge" : "latest", "ios" : "7+", "an" : "2.1+ (except 3.x)"}
+       */
+      function Component() {
+        /**
+         * @deprecated
+         */
+        this.options = {};
+        this._eventHandler = {};
+      }
+
+      var __proto = Component.prototype;
+
+      __proto.trigger = function (eventName, customEvent) {
+        var _this = this;
+
+        var restParam = [];
+
+        for (var _i = 2; _i < arguments.length; _i++) {
+          restParam[_i - 2] = arguments[_i];
+        }
+
+        var handlerList = this._eventHandler[eventName] || [];
+        var hasHandlerList = handlerList.length > 0;
+
+        if (!hasHandlerList) {
+          return true;
+        }
+
+        if (!customEvent) {
+          customEvent = {};
+        } // If detach method call in handler in first time then handler list calls.
+
+
+        handlerList = handlerList.concat();
+        var isCanceled = false; // This should be done like this to pass previous tests
+
+        customEvent.eventType = eventName;
+
+        customEvent.stop = function () {
+          isCanceled = true;
+        };
+
+        customEvent.currentTarget = this;
+        var arg = [customEvent];
+
+        if (restParam.length >= 1) {
+          arg = arg.concat(restParam);
+        }
+
+        handlerList.forEach(function (handler) {
+          handler.apply(_this, arg);
+        });
+        return !isCanceled;
+      };
+
+      __proto.once = function (eventName, handlerToAttach) {
+        var _this = this;
+
+        if (typeof eventName === "object" && isUndefined(handlerToAttach)) {
+          var eventHash = eventName;
+
+          for (var key in eventHash) {
+            this.once(key, eventHash[key]);
+          }
+
+          return this;
+        } else if (typeof eventName === "string" && typeof handlerToAttach === "function") {
+          var listener_1 = function () {
+            var args = [];
+
+            for (var _i = 0; _i < arguments.length; _i++) {
+              args[_i] = arguments[_i];
+            }
+
+            handlerToAttach.apply(_this, args);
+
+            _this.off(eventName, listener_1);
+          };
+
+          this.on(eventName, listener_1);
+        }
+
+        return this;
+      };
+      /**
+       * Checks whether an event has been attached to a component.
+       * @ko 컴포넌트에 이벤트가 등록됐는지 확인한다.
+       * @param {String} eventName The name of the event to be attached <ko>등록 여부를 확인할 이벤트의 이름</ko>
+       * @return {Boolean} Indicates whether the event is attached. <ko>이벤트 등록 여부</ko>
+       * @example
+       * class Some extends eg.Component {
+       *   some() {
+       *     this.hasOn("hi");// check hi event.
+       *   }
+       * }
+       */
+
+
+      __proto.hasOn = function (eventName) {
+        return !!this._eventHandler[eventName];
+      };
+
+      __proto.on = function (eventName, handlerToAttach) {
+        if (typeof eventName === "object" && isUndefined(handlerToAttach)) {
+          var eventHash = eventName;
+
+          for (var name in eventHash) {
+            this.on(name, eventHash[name]);
+          }
+
+          return this;
+        } else if (typeof eventName === "string" && typeof handlerToAttach === "function") {
+          var handlerList = this._eventHandler[eventName];
+
+          if (isUndefined(handlerList)) {
+            this._eventHandler[eventName] = [];
+            handlerList = this._eventHandler[eventName];
+          }
+
+          handlerList.push(handlerToAttach);
+        }
+
+        return this;
+      };
+
+      __proto.off = function (eventName, handlerToDetach) {
+        var e_1, _a; // Detach all event handlers.
+
+
+        if (isUndefined(eventName)) {
+          this._eventHandler = {};
+          return this;
+        } // Detach all handlers for eventname or detach event handlers by object.
+
+
+        if (isUndefined(handlerToDetach)) {
+          if (typeof eventName === "string") {
+            delete this._eventHandler[eventName];
+            return this;
+          } else {
+            var eventHash = eventName;
+
+            for (var name in eventHash) {
+              this.off(name, eventHash[name]);
+            }
+
+            return this;
+          }
+        } // Detach single event handler
+
+
+        var handlerList = this._eventHandler[eventName];
+
+        if (handlerList) {
+          var idx = 0;
+
+          try {
+            for (var handlerList_1 = __values(handlerList), handlerList_1_1 = handlerList_1.next(); !handlerList_1_1.done; handlerList_1_1 = handlerList_1.next()) {
+              var handlerFunction = handlerList_1_1.value;
+
+              if (handlerFunction === handlerToDetach) {
+                handlerList.splice(idx, 1);
+                break;
+              }
+
+              idx++;
+            }
+          } catch (e_1_1) {
+            e_1 = {
+              error: e_1_1
+            };
+          } finally {
+            try {
+              if (handlerList_1_1 && !handlerList_1_1.done && (_a = handlerList_1.return)) _a.call(handlerList_1);
+            } finally {
+              if (e_1) throw e_1.error;
+            }
+          }
+        }
+
+        return this;
+      };
+      /**
+       * Version info string
+       * @ko 버전정보 문자열
+       * @name VERSION
+       * @static
+       * @type {String}
+       * @example
+       * eg.Component.VERSION;  // ex) 2.0.0
+       * @memberof eg.Component
+       */
+
+
+      Component.VERSION = "2.2.0";
+      return Component;
+    }();
+
+    return Component;
+
+})));
+//# sourceMappingURL=component.js.map
