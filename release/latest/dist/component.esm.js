f95a38a220c81b6e3183507f209be0e4ecf016c3
WoodNeck <saiyu915@naver.com>
2020-10-16 12:32:09 +0900

    Updates

--- release/latest/dist/component.esm.js
+++ release/latest/dist/component.esm.js
@@ -61,10 +61,43 @@ function () {
   function Component() {
     /**
      * @deprecated
+     * @private
      */
     this.options = {};
     this._eventHandler = {};
   }
+  /**
+   * Triggers a custom event.
+   * @ko 커스텀 이벤트를 발생시킨다
+   * @param {string} eventName The name of the custom event to be triggered <ko>발생할 커스텀 이벤트의 이름</ko>
+   * @param {object} customEvent Event data to be sent when triggering a custom event <ko>커스텀 이벤트가 발생할 때 전달할 데이터</ko>
+   * @param {any[]} restParam Additional parameters when triggering a custom event <ko>커스텀 이벤트가 발생할 때 필요시 추가적으로 전달할 데이터</ko>
+   * @return Indicates whether the event has occurred. If the stop() method is called by a custom event handler, it will return false and prevent the event from occurring. <a href="https://github.com/naver/egjs-component/wiki/How-to-make-Component-event-design%3F">Ref</a> <ko>이벤트 발생 여부. 커스텀 이벤트 핸들러에서 stop() 메서드를 호출하면 'false'를 반환하고 이벤트 발생을 중단한다. <a href="https://github.com/naver/egjs-component/wiki/How-to-make-Component-event-design%3F">참고</a></ko>
+   * @example
+   * ```
+   * class Some extends eg.Component {
+   *   some(){
+   *     if(this.trigger("beforeHi")){ // When event call to stop return false.
+   *       this.trigger("hi");// fire hi event.
+   *     }
+   *   }
+   * }
+   *
+   * const some = new Some();
+   * some.on("beforeHi", (e) => {
+   *   if(condition){
+   *     e.stop(); // When event call to stop, `hi` event not call.
+   *   }
+   * });
+   * some.on("hi", (e) => {
+   *   // `currentTarget` is component instance.
+   *   console.log(some === e.currentTarget); // true
+   * });
+   * // If you want to more know event design. You can see article.
+   * // https://github.com/naver/egjs-component/wiki/How-to-make-Component-event-design%3F
+   * ```
+   */
+
 
   var __proto = Component.prototype;
 
@@ -110,6 +143,31 @@ function () {
     });
     return !isCanceled;
   };
+  /**
+   * Executed event just one time.
+   * @ko 이벤트가 한번만 실행된다.
+   * @param {string} eventName The name of the event to be attached <ko>등록할 이벤트의 이름</ko>
+   * @param {function} handlerToAttach The handler function of the event to be attached <ko>등록할 이벤트의 핸들러 함수</ko>
+   * @return An instance of a component itself<ko>컴포넌트 자신의 인스턴스</ko>
+   * @example
+   * ```
+   * class Some extends eg.Component {
+   * hi() {
+   *   alert("hi");
+   * }
+   * thing() {
+   *   this.once("hi", this.hi);
+   * }
+   *
+   * var some = new Some();
+   * some.thing();
+   * some.trigger("hi");
+   * // fire alert("hi");
+   * some.trigger("hi");
+   * // Nothing happens
+   * ```
+   */
+
 
   __proto.once = function (eventName, handlerToAttach) {
     var _this = this;
@@ -143,20 +201,41 @@ function () {
   /**
    * Checks whether an event has been attached to a component.
    * @ko 컴포넌트에 이벤트가 등록됐는지 확인한다.
-   * @param {String} eventName The name of the event to be attached <ko>등록 여부를 확인할 이벤트의 이름</ko>
-   * @return {Boolean} Indicates whether the event is attached. <ko>이벤트 등록 여부</ko>
+   * @param {string} eventName The name of the event to be attached <ko>등록 여부를 확인할 이벤트의 이름</ko>
+   * @return {boolean} Indicates whether the event is attached. <ko>이벤트 등록 여부</ko>
    * @example
+   * ```
    * class Some extends eg.Component {
    *   some() {
    *     this.hasOn("hi");// check hi event.
    *   }
    * }
+   * ```
    */
 
 
   __proto.hasOn = function (eventName) {
     return !!this._eventHandler[eventName];
   };
+  /**
+   * Attaches an event to a component.
+   * @ko 컴포넌트에 이벤트를 등록한다.
+   * @param {string} eventName The name of the event to be attached <ko>등록할 이벤트의 이름</ko>
+   * @param {function} handlerToAttach The handler function of the event to be attached <ko>등록할 이벤트의 핸들러 함수</ko>
+   * @return An instance of a component itself<ko>컴포넌트 자신의 인스턴스</ko>
+   * @example
+   * ```
+   * class Some extends eg.Component {
+   *   hi() {
+   *     console.log("hi");
+   *   }
+   *   some() {
+   *     this.on("hi",this.hi); //attach event
+   *   }
+   * }
+   * ```
+   */
+
 
   __proto.on = function (eventName, handlerToAttach) {
     if (typeof eventName === "object" && isUndefined(handlerToAttach)) {
@@ -180,6 +259,25 @@ function () {
 
     return this;
   };
+  /**
+   * Detaches an event from the component.
+   * @ko 컴포넌트에 등록된 이벤트를 해제한다
+   * @param {string} eventName The name of the event to be detached <ko>해제할 이벤트의 이름</ko>
+   * @param {function} handlerToDetach The handler function of the event to be detached <ko>해제할 이벤트의 핸들러 함수</ko>
+   * @return An instance of a component itself <ko>컴포넌트 자신의 인스턴스</ko>
+   * @example
+   * ```
+   * class Some extends eg.Component {
+   *   hi() {
+   *     console.log("hi");
+   *   }
+   *   some() {
+   *     this.off("hi",this.hi); //detach event
+   *   }
+   * }
+   * ```
+   */
+
 
   __proto.off = function (eventName, handlerToDetach) {
     var e_1, _a; // Detach all event handlers.
@@ -243,7 +341,6 @@ function () {
    * @ko 버전정보 문자열
    * @name VERSION
    * @static
-   * @type {String}
    * @example
    * eg.Component.VERSION;  // ex) 2.0.0
    * @memberof eg.Component
