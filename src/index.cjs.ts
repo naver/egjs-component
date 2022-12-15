a517a13ad9b2e9fa6c660adea3ede5218763bdf3
Daybrush <younkue.choi@navercorp.com>
2022-12-15 16:27:03 +0900

    fix: fix commonjs behavior

--- src/index.cjs.ts
+++ src/index.cjs.ts
@@ -1,5 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /*
  * Copyright (c) 2015 NAVER Corp.
  * egjs projects are licensed under the MIT license
@@ -9,4 +9,8 @@ import Component, * as modules from "./index";
 for (const name in modules) {
   (Component as any)[name] = (modules as any)[name];
 }
+
+declare const module: any;
+module.exports = Component;
 export default Component;
+export * from "./index";
